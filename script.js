6e65abb466924134939cb7ae2199932bff95393d
Daniel F Vieira <danidani7896as@gmail.com>
2026-07-13 17:57:13 -0300

    Primeira Versão
    Projeto inicial completo. Ainda irei implementar a função de ler códigos de barra fisicamente como nos supermercados.

---
+++ script.js
@@ -0,0 +1,28 @@
+function consultar() {
+    let cod = Number(document.querySelector('#cod').value)
+    fetch("data.json")
+        .then(response => response.json())
+        .then(produtos => {
+            const nome = document.querySelector('#nome')
+            const imagem = document.querySelector('#imagem')
+            const codigo = document.querySelector('#codigo')
+            const preco = document.querySelector('#preco')
+            const fechar = document.querySelector('.fechar')
+            let conteiner = document.querySelector('#conteiner')
+            let produto = produtos.find(produto => produto.codigo === cod)
+            if (produto) {
+                nome.textContent = produto.nome
+                imagem.src = produto.imagem
+                imagem.alt = produto.nome
+                codigo.textContent = `Código: ${produto.codigo}`
+                preco.textContent = produto.preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+                conteiner.classList.remove("hidden")
+                fechar.onclick = () => {
+                    conteiner.classList.add("hidden")
+                }
+            } else {
+                window.alert('Produto não encontrado! Tente novamente.')
+            }
+        })
+}
+document.querySelector('#button').addEventListener("click", consultar)
